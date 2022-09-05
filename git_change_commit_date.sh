9e216bc26e314f1e70dc22b21f3386c4db8560cb
Wahyu Kurniawan <ayungavis@gmail.com>
2022-09-05 10:19:19 +0700

    🐛 fix: git change commit date not work properly
    When changing the commit date, the date still on the current date

--- git_change_commit_date.sh
+++ git_change_commit_date.sh
@@ -2,8 +2,11 @@
 # Get current date
 CURRENT_DATE=$(date)
 
+# Set the default time
+TIME="10:19:19 WIB"
+
 # Input the date in the format Mon 12 Aug 2022
 DATE=$(gum input --placeholder "Insert the date (Current date: $CURRENT_DATE)")
 
 # Change the date of the last commit
-git commit --amend --date="$DATE 10:19:19 WIB"
+GIT_COMMITTER_DATE="$DATE $TIME" git commit --amend --date="$DATE $TIME"
