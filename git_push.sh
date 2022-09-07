307114a257112236dc16c5c4b67f4c99cca10705
Wahyu Kurniawan <ayungavis@gmail.com>
2022-09-07 10:19:19 +0700

    ⚡️ feat: only show current branch
    Update the git branch command to only show the current branch

--- git_push.sh
+++ git_push.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Choose the branch before pushing
-BRANCH=$(gum choose $(git branch --format='%(refname:short)'))
+BRANCH=$(gum choose $(git branch --show-current))
 
 # Push the branch
 gum confirm "Push $BRANCH?" && git push origin $BRANCH
