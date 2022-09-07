82aba4f71564f9c3c5e3800e88b94c09e2dd0c2e
Wahyu Kurniawan <ayungavis@gmail.com>
2022-09-07 10:19:19 +0700

    ⚡️ feat: add change date
    Add change date confirmation after commit the file

--- git_commit.sh
+++ git_commit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-TYPE=$(cat commit_type.txt | gum filter --limit 1)
+TYPE=$(cat ~/programming/tranityproject/shell/commit_type.txt | gum filter --limit 1)
 
 # Pre-populate the input with the type(scope): so that the user may change it
 SUMMARY=$(gum input --value "$TYPE: " --placeholder "Summary of this change")
@@ -7,3 +7,6 @@ DESCRIPTION=$(gum write --placeholder "Details of this change (CTRL+D to finish)
 
 # Commit these changes
 gum confirm "Commit changes?" && git commit -m "$SUMMARY" -m "$DESCRIPTION"
+
+# Change the commit date
+gum confirm "Do you want to change the date?" && gum style --foreground 212 '=====[Change the commit date]=====' && ~/programming/tranityproject/shell/git_change_commit_date.sh
