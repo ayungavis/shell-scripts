54ebb176b115bf9ac30d30cf8c397f6c6539c83f
Wahyu Kurniawan <ayungavis@gmail.com>
2022-10-02 10:19:19 +0700

    🐛 fix: update git commit path
    Update the `git_commit.sh` wrong path

--- git_add.sh
+++ git_add.sh
@@ -9,7 +9,7 @@ gum confirm "Add $FILES?" && git add $FILES
 gum style --foreground 212 '=====[Git Status]=====' && git status
 
 # Commit these changes
-gum style --foreground 212 '=====[Commit the staging files]=====' && ./git_commit.sh
+gum style --foreground 212 '=====[Commit the staging files]=====' && ~/programming/tranityproject/shell/git_commit.sh
 
 # Change the commit date
 gum confirm "Do you want to change the date?" && gum style --foreground 212 '=====[Change the commit date]=====' && ~/programming/tranityproject/shell/git_change_commit_date.sh
